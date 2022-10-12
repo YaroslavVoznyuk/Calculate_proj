33963d4aa4a661539b3320181d9b21a45da972da
VladimirTkachenko3010 <v.s.tkachenko@student.khai.edu>
2022-10-12 18:30:20 +0300

    repull

--- CalcWinForm/Form1.Designer.cs
+++ CalcWinForm/Form1.Designer.cs
@@ -77,7 +77,6 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(638, 59);
             this.label1.TabIndex = 0;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // buttonPlusMinus
             // 
